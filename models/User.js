6a8654a1677b0d8bc7f17baa3edc28f68c094916
Siddharth <officialsiddharthbisht@gmail.com>
2022-12-11 10:22:35 +0530

    ADD: User Schema

--- models/User.js
+++ models/User.js
@@ -1,7 +1,43 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-
+    name: {
+        type: String,
+        require: [true, "Please give a name"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please give an email"],
+        unique: [true, "Email already exists"],
+    },
+    password: {
+        type: String,
+        required: [true, "Please give a password"],
+        minLength: [8, "Password must be at least 8 characters long"],
+        select: false,
+    },
+    post: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post",
+        },
+    ],
+    followers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        }
+    ],
+    following: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        }
+    ],
+    avatar: {
+        public_id: String,
+        url: String,
+    }
 });
 
 
