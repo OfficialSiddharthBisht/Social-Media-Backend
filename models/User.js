a7d4e89e057a00f6b57b3909b7ce1ccf47bdbce1
Siddharth <officialsiddharthbisht@gmail.com>
2022-12-13 09:02:09 +0530

    ADD: Password Hashing using bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -40,5 +41,11 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 12);
+    }
+    next();
+})
 
 module.exports = mongoose.model("User", userSchema);
